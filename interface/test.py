dd86fef1568b910be412911622028a1fe44a0a00
Shawn Ray <asmodasis@gmail.com>
2020-11-30 19:56:26 -0800

    worked on cancerdetection/Image/index.php, for image input and the driver

--- interface/test.py
+++ interface/test.py
@@ -3,8 +3,12 @@ from driver import driver
 
 def main():
     x = driver("Breast", False, "G:/Senior Project/dataset/Breast/prepared/train/0/8863_idx5_x151_y1301_class0.png")
-    print("Value returned from driver : ")
+    print("Value x returned from driver : ")
     print(x)
+
+    y = driver("Breast", False, "G:/Senior Project/dataset/Breast/prepared/train/1/8863_idx5_x151_y1301_class0.png")
+    print("Value y returned from driver : ")
+    print(y)
 
 if __name__ == "__main__":
     main()
