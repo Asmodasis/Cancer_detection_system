3b77ad4fd7fa33944ff08fd902f78b938acac068
christian-greenman <74841252+christian-greenman@users.noreply.github.com>
2021-03-13 16:36:24 -0800

    Update index.php

--- frontend/Image/index.php
+++ frontend/Image/index.php
@@ -18,7 +18,7 @@
                 <p><input type="file"  accept="image/*" name="image" id="file" onchange="loadFile(event)"  style="display: none;"></p>
                 <p><label for="file" style="cursor: pointer;">Upload Image</label></p>
                 <p><img id="output" width="200" /></p>
-                <button id = "modelButton" onclick="runModel(event)"> Click here to run the model </button>
+                <button id = "modelButton"> Click here to run the model </button>
                 <!-- /.box -->
               </div>
             </div>';
@@ -88,28 +88,32 @@
 	  image.src = URL.createObjectURL(event.target.files[0]);
 
   }
+  
+  
+  //====================================================================================
+  //  Required usage:
+  //                INSTALL: npm install @tensorflow/tfjs
+  //                SCRIPT: <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js">
+  //                NPM: import * as tf from '@tensorflow/tfjs';
+  //=====================================================================================
+  
   </script>
-  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js">
-  /*====================================================================================
-    Required usage:
-                  INSTALL: npm install @tensorflow/tfjs
-                  SCRIPT: <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js">
-                  NPM: import * as tf from '@tensorflow/tfjs';
-  =====================================================================================*/
-
+  <script type="module" src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js">
+  console.log("Model");
+  $("#modelButton").click(runModel);
+  
   async function runModel(event){
-
-    
+	//import * as tf from '@tensorflow/tfjs-node'
         alert("TEST Model Ran")
       
         var whatOrgan = document.getElementById('Organ');             // The organ selected from the drop down
         
         var img_width = 200;                                          // The size of the images
         var img_height = 200;
-        import * as tf from '@tensorflow/tfjs-node'
+        
                                                                       // Import the image as a tensor object
-      /*
-        var im = tf.keras.preprocessing.image.load_img(img,, grayscale=False, color_mode='rgb', target_size=(img_width, img_height));
+      
+        var im = tf.keras.preprocessing.image.load_img(img, grayscale=False, color_mode='rgb', target_size=(img_width, img_height));
         
         var x = tf.keras.preprocessing.image.img_to_array(im);        // Convert the image to an array
         //var x = tf.expand_dims(x, axis=0);                            
@@ -125,13 +129,13 @@
 
             return model.predict(x);  
         }else{                                                         // Else a normal network will be applied
-            var file = '../'+whatOrgan+'/'+whatOrgan+'_Model.h5';
+            var file = whatOrgan+'/'+whatOrgan+'_Model.h5';
             
             var model = await tf.loadLayersModel(file);               // Load the model
 
             alert(model.predict(x)); 
         }
-        */
+        
   }
   
 </script>
